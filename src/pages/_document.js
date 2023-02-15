390354f65d976652e675c4a13f386459a57f7b2b
rlawndks4 <rlawndks0423@naver.com>
2023-02-15 21:43:12 +0900

    fit: 디자인 변경

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,16 +13,28 @@ import { createEmotionCache } from 'src/@core/utils/create-emotion-cache'
 class CustomDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang='ko'>
         <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
-          <link
-            rel='stylesheet'
-            href='https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'
+          <title>{`comagain`}</title>
+          <meta
+            name='description'
+            content={`comagain`}
           />
-          <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
-          <link rel='shortcut icon' href='/images/favicon.png' />
+          <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
+          <meta name='viewport' content='initial-scale=1, width=device-width' />
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="{{ $name }}" />
+          <meta property="og:image" content="{{ $favicon_img }}" />
+          <meta property="og:description" content="모두가 간편한 서비스 {{ $name }}" />
+          <meta name="author" content="purplevery" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-title" content="{{ $name }}" />
+          <meta name="theme-color" content="{{ $bk_clr }}" />
+          <link rel="apple-touch-icon" sizes="180x180" href="{{ $favicon_img }}" />
+          <link rel="shortcut icon" href="{{ $favicon_img }}" type="image/x-icon" />
         </Head>
         <body>
           <Main />
@@ -39,12 +51,12 @@ CustomDocument.getInitialProps = async ctx => {
   ctx.renderPage = () =>
     originalRenderPage({
       enhanceApp: App => props =>
-        (
-          <App
-            {...props} // @ts-ignore
-            emotionCache={cache}
-          />
-        )
+      (
+        <App
+          {...props} // @ts-ignore
+          emotionCache={cache}
+        />
+      )
     })
   const initialProps = await Document.getInitialProps(ctx)
   const emotionStyles = extractCriticalToChunks(initialProps.html)
