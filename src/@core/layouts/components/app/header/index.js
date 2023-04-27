7ee386df7cecdfa8589971811e9f59a2ad81e04c
rlawndks4 <rlawndks0423@naver.com>
2023-04-28 02:36:39 +0900

    feat: 대량 업로드

--- src/@core/layouts/components/app/header/index.js
+++ src/@core/layouts/components/app/header/index.js
@@ -129,12 +129,7 @@ const Header = () => {
       let dns_data = await getLocalStorage(LOCALSTORAGE.DNS_DATA);
       dns_data = JSON.parse(dns_data);
       setDnsData(dns_data);
-      const response = await axiosIns().get(`/api/v1/shop/items?page=1&page_size=1000000&s_dt=1900-01-01&e_dt=2500-01-01&cate_id=0&brand_id=${dns_data?.id}&search=`);
-      if (response?.data?.categories.length == 0) {
-        toast.error("카테고리를 먼저 등록해 주세요.");
-        router.back();
-      }
-      setCategoryList(response?.data?.categories);
+
     } catch (err) {
       let push_lick = await processCatch(err);
       if (push_lick == -1) {
