2dc446500140c3d175f1c37db5e92fd6cafebdd7
rlawndks4 <rlawndks0423@naver.com>
2023-04-30 20:58:38 +0900

    feat: 다크모스 색상 설정

---
+++ src/pages/app/merchandise/list.js
@@ -0,0 +1,10 @@
+import AppLayout from "src/@core/layouts/AppLayout"
+
+const List = () => {
+  return (
+    <>
+    </>
+  )
+}
+List.getLayout = page => <AppLayout>{page}</AppLayout>
+export default List
