b64083d658c43a46b8ea414070a075d09e5f5093
rlawndks4 <rlawndks0423@naver.com>
2023-06-24 15:24:25 +0900

    feat: 업데이트

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -159,29 +159,29 @@ const App = props => {
     </>
   )
 }
-if (typeof window == 'undefined') {
-  App.getInitialProps = async ({ Component, ctx }) => {
-    try {
-      const pageProps = Component.getInitialProps
-        ? await Component.getInitialProps(ctx)
-        : {};
-      if (ctx.req?.headers) {
-        const host = ctx.req.headers.host.split(':')[0];
-        const res = await fetch(`${process.env.BACK_URL}/api/v1/auth/domain?dns=${host}`);
-        const json = (await res.json());
-        return {
-          dns_data: json
-        }
-      } else {
-        return {
-          dns_data: {}
-        }
-      }
-    } catch (err) {
-      return {
-        dns_data: {}
-      }
-    }
-  }
-}
+// if (typeof window == 'undefined') {
+//   App.getInitialProps = async ({ Component, ctx }) => {
+//     try {
+//       const pageProps = Component.getInitialProps
+//         ? await Component.getInitialProps(ctx)
+//         : {};
+//       if (ctx.req?.headers) {
+//         const host = ctx.req.headers.host.split(':')[0];
+//         const res = await fetch(`${process.env.BACK_URL}/api/v1/auth/domain?dns=${host}`);
+//         const json = (await res.json());
+//         return {
+//           dns_data: json
+//         }
+//       } else {
+//         return {
+//           dns_data: {}
+//         }
+//       }
+//     } catch (err) {
+//       return {
+//         dns_data: {}
+//       }
+//     }
+//   }
+// }
 export default App
