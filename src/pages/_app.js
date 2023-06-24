fc2698505a75e6055ea6ca1ea0718f6a38b77e2b
rlawndks4 <rlawndks0423@naver.com>
2023-06-24 15:52:30 +0900

    feat: 업데이트

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -159,29 +159,33 @@ const App = props => {
     </>
   )
 }
-// if (typeof window == 'undefined') {
-//   App.getInitialProps = async ({ Component, ctx }) => {
-//     try {
-//       const pageProps = Component.getInitialProps
-//         ? await Component.getInitialProps(ctx)
-//         : {};
-//       if (ctx.req?.headers) {
-//         const host = ctx.req.headers.host.split(':')[0];
-//         const res = await fetch(`${process.env.BACK_URL}/api/v1/auth/domain?dns=${host}`);
-//         const json = (await res.json());
-//         return {
-//           dns_data: json
-//         }
-//       } else {
-//         return {
-//           dns_data: {}
-//         }
-//       }
-//     } catch (err) {
-//       return {
-//         dns_data: {}
-//       }
-//     }
-//   }
-// }
+App.getInitialProps = async (context) => {
+  const { ctx, Component } = context;
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    // Component (pages 폴더에 있는 컴포넌트)에 getInitialProps가 있다면
+    pageProps = (await Component.getInitialProps(ctx)) || {};
+  }
+  try {
+    if (ctx.req?.headers) {
+      const host = ctx.req.headers.host.split(':')[0];
+      const res = await fetch(`${process.env.BACK_URL}/api/v1/auth/domain?dns=${host}`);
+      const json = (await res.json());
+      return {
+        dns_data: json
+      }
+    } else {
+      return {
+        dns_data: {},
+        pageProps
+      }
+    }
+  } catch (err) {
+    return {
+      dns_data: {},
+      pageProps
+    }
+  }
+}
 export default App
--- src/pages/shop/seller/[id].js
+++
@@ -1,8 +0,0 @@
-
-const Seller = () => {
-  return (
-    <>
-
-    </>
-  )
-}
